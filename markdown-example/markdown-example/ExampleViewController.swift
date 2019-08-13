ae79b822b9548e2bc5708fb99e5e1291d44fc208
John Garrett <jgarrett@kabbage.com>
2019-08-13 19:17:35 -0400

    example screen completion

--- markdown-example/markdown-example/ExampleViewController.swift
+++ markdown-example/markdown-example/ExampleViewController.swift
@@ -2,7 +2,11 @@ import UIKit
 
 class ExampleViewController: UIViewController {
     @IBOutlet var textView: UITextView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private let renderer = MarkdownRenderer(baseFont: .systemFont(ofSize: 16))
+}
+
+extension ExampleViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        textView.attributedText = renderer.render(text: textView.text)
     }
 }
